e1a092895e314c1852d477b7faa338dcf45bdfaf
barakat95 <mohammedbarakat75@gmail.com>
2021-02-05 17:08:45 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,117 @@
+(function ($) {
+  "use strict";
+
+  $(".portfolio-single-slider").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $(".clients-logo").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $(".testimonial-wrap").slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    infinite: true,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".portfolio-gallery").each(function () {
+    $(this)
+      .find(".popup-gallery")
+      .magnificPopup({
+        type: "image",
+        gallery: {
+          enabled: true,
+        },
+      });
+  });
+
+  var map;
+
+  function initialize() {
+    var mapOptions = {
+      zoom: 13,
+      center: new google.maps.LatLng(50.97797382271958, -114.107718560791),
+      // styles: style_array_here
+    };
+    map = new google.maps.Map(
+      document.getElementById("map-canvas"),
+      mapOptions
+    );
+  }
+
+  var google_map_canvas = $("#map-canvas");
+
+  if (google_map_canvas.length) {
+    google.maps.event.addDomListener(window, "load", initialize);
+  }
+
+  // Counter
+
+  $(".counter-stat").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+
+  let offsetContent = jQuery(".section-markiting").offset().top;
+  let offsetFooter = jQuery(".footer").offset().footer;
+
+  $(document).scroll(function () {
+    let y = $(this).scrollTop();
+
+    // console.log('offsetContent');
+    // console.log(offsetContent);
+    // console.log('y');
+    // // console.log(y);
+    // if (y > offsetContent) {
+    //   console.log("reached");
+    //   console.log("reached");
+    //   console.log("reached");
+    //   $(".service-markiting").animate({left: '250px'});
+    // } else {
+    //     // $('.service-markiting').hide();
+    // }
+  });
+})(jQuery);
